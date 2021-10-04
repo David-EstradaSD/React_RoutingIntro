4c1bdfbbb581b3a7c845236423e3f2cf79ba7e00
David <destrada2193@gmail.com>
2021-10-04 14:16:38 -0700

    Create <Header> file for navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { Route } from "react-router-dom";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Route path='/welcome' >
-        <Welcome />
-      </Route>
-      <Route path='/products' >
-        <Products />
-      </Route>
+      <Header />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
 
-
-// our-domain.com/welcome --> Welcome.js component 
-// our-domain.com/products --> Produts.js component 
+// our-domain.com/welcome --> Welcome.js component
+// our-domain.com/products --> Produts.js component
