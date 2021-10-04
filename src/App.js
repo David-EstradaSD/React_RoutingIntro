e89990f8e709157b0ae5683024679706370548cc
David <destrada2193@gmail.com>
2021-10-04 14:06:53 -0700

    Setup dummy components and import Route and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path='/welcome' >
+        <Welcome />
+      </Route>
+      <Route path='/products' >
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+
+// our-domain.com/welcome --> Welcome.js component 
+// our-domain.com/products --> Produts.js component 
